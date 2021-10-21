31bb3e7f1258733b0a39b197053cbad87f23eeda
Adit Roychowdhury <rcadit@gmail.com>
2021-10-21 04:56:42 +0100

    removed CatkinIgnore

--- interbotix_ros_xshexapods/examples/python_demos/move_in_place.py
+++ interbotix_ros_xshexapods/examples/python_demos/move_in_place.py
@@ -8,54 +8,58 @@ from interbotix_xs_modules.hexapod import InterbotixHexapodXS
 # Then change to this directory and type 'python rotate_in_place.py'
 
 def main():
-
-    bot = InterbotixHexapodXS('wxmark4')
-    bot.hex.move_in_place(z=0.1)
+    print("starting")
+    bot = InterbotixHexapodXS('pxmark4')
+    
+    bot.hex.move_in_place(z=0.4)
     bot.hex.modify_stance(-0.02)
 
-    # translate in X
-    for step in range(51):
-        x = 0.05 * math.sin(2*math.pi * step/50.0)
-        bot.hex.move_in_place(x=x, moving_time=0.15, blocking=False)
-        time.sleep(0.04)
-    time.sleep(0.2)
+    # # translate in X
+    # for step in range(100):
+    #     print("x")
+    #     x = 0.05 * step/50
+    #     bot.hex.move_in_place(x=x, moving_time=0.15, blocking=False)
+    #     time.sleep(0.04)
+    # time.sleep(0.2)
 
-    # translate in Y
-    for step in range(51):
-        y = 0.05 * math.sin(2*math.pi * step/50.0)
-        bot.hex.move_in_place(y=y, moving_time=0.15, blocking=False)
-        time.sleep(0.04)
-    time.sleep(0.2)
+    # # translate in Y
+    # for step in range(51):
+    #     print("y")
+    #     y = 0.05 * math.sin(2*math.pi * step/50.0)
+    #     bot.hex.move_in_place(y=y, moving_time=0.15, blocking=False)
+    #     time.sleep(0.04)
+    # time.sleep(0.2)
 
     # translate in Z
     for step in range(51):
+        print("z")
         z = 0.1 + 0.05 * math.sin(2*math.pi * step/50.0)
         bot.hex.move_in_place(z=z, moving_time=0.15, blocking=False)
         time.sleep(0.04)
     time.sleep(0.2)
 
-    # roll around X
-    for step in range(51):
-        roll = 0.3 * math.sin(2*math.pi * step/50.0)
-        bot.hex.move_in_place(roll=roll, moving_time=0.15, blocking=False)
-        time.sleep(0.04)
-    time.sleep(0.2)
+    # # roll around X
+    # for step in range(51):
+    #     roll = 0.3 * math.sin(2*math.pi * step/50.0)
+    #     bot.hex.move_in_place(roll=roll, moving_time=0.15, blocking=False)
+    #     time.sleep(0.04)
+    # time.sleep(0.2)
 
-    # pitch around Y
-    for step in range(51):
-        pitch = 0.3 * math.sin(2*math.pi * step/50.0)
-        bot.hex.move_in_place(pitch=pitch, moving_time=0.15, blocking=False)
-        time.sleep(0.04)
-    time.sleep(0.2)
+    # # pitch around Y
+    # for step in range(51):
+    #     pitch = 0.3 * math.sin(2*math.pi * step/50.0)
+    #     bot.hex.move_in_place(pitch=pitch, moving_time=0.15, blocking=False)
+    #     time.sleep(0.04)
+    # time.sleep(0.2)
 
-    # yaw around Z
-    for step in range(51):
-        yaw = 0.3 * math.sin(2*math.pi * step/50.0)
-        bot.hex.move_in_place(yaw=yaw, moving_time=0.15, blocking=False)
-        time.sleep(0.04)
-    time.sleep(0.2)
+    # # yaw around Z
+    # for step in range(51):
+    #     yaw = 0.3 * math.sin(2*math.pi * step/50.0)
+    #     bot.hex.move_in_place(yaw=yaw, moving_time=0.15, blocking=False)
+    #     time.sleep(0.04)
+    # time.sleep(0.2)
 
-    bot.hex.reset_hexapod('sleep')
+    # bot.hex.reset_hexapod('sleep')
 
 if __name__=='__main__':
     main()
